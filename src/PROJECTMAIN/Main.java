3aa5a07ccb1f1e3822187db938350a4ab11188f2
김태우 <70886438+su-9woo@users.noreply.github.com>
2024-11-21 09:16:48 +0900

    Delete src directory

--- src/PROJECTMAIN/Main.java
+++
@@ -1,9 +0,0 @@
-package PROJECTMAIN;
-
-public class Main {
-    public static void main(String[] args) {
-        MainApplication MainApplication = new MainApplication();
-        MainApplication.startApplication();
-    }
-}
-
